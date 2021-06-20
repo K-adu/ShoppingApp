e7992c083b879dcb4d1e4368f84a4e903add7d85
K-adu <manishbaral112@gmail.com>
2021-06-20 14:37:31 +0545

    initial commit

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,46 @@
+import 'package:e_commerce/screens/homepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final tabs = [
+    Center(child: HomePage()),
+    Center(child: Text('Maps')),
+    Center(child: Text('Blogs')),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.greenAccent,
+        currentIndex: _currentIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_max_sharp),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_sharp),
+            label: 'map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Blogs',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
